e3c3552f4b8046629cc8ecf80a952b756b72d61d
PAUL SCHOENFELDER <schoenfelder.p@gmail.com>
2015-10-21 19:54:30 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='named-dates',
+      version='0.1',
+      description='TODO',
+      url='https://github.com/pschoenfelder/named-dates',
+      author='Paul Schoenfelder',
+      author_email='TODO',
+      license='MIT',
+      packages=['pytest'])
