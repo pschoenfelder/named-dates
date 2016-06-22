21eadcf7fbbe7f618a37580be668c1600554fbd8
PAUL SCHOENFELDER <schoenfelder.p@gmail.com>
2016-06-21 23:13:56 -0400

    Updated setup.py metadata

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(name='named-dates',
       version='0.1',
-      description='TODO',  # TODO
+      description='Date logic for those referred to by name',
       url='https://github.com/pschoenfelder/named-dates',
+      download_url='https://github.com/pschoenfelder/named-dates/tarball/0.1',
       author='Paul Schoenfelder',
-      author_email='TODO',  # TODO
+      author_email='schoenfelder.p@gmail.com',
       license='MIT',
       packages=['named_dates'],
       requires=['pytest'])
